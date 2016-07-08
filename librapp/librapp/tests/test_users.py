629189df4a98450fea998c4248d75bcd5971aa74
Prabin Gautam <gautam.prabin@gmail.com>
2016-07-07 23:23:16 -0500

    cleanup

--- librapp/librapp/tests/test_users.py
+++ librapp/librapp/tests/test_users.py
@@ -6,23 +6,7 @@ class UsersTest(APITestCase):
     def setUp(self):
         self.path = '/users/'
 
-        self.user1 = {
-            'first_name': 'unit-test-firstname1',
-            'last_name':'unit-test-lastname1',
-            'email':'unit-test-emails1@utdallas.edu',
-            'password':'unit-test-password1'
-		}
-        self._setup_user(row=self.user1)
-
-    def __setup_user(self,row=None):
-
-        try:
-            models.User.objects.get(email=row['email'])
-        except models.User.DoesNotExist:
-            models.User.objects.create(**row)
-
     def test_update_user_success(self):
-
         """
         Ensure we can create new user
         :return:
